d93027d6e3834c9ebdfa3caa8ffcb4aaf37a77cc
오승철 <tmdcjf103@gmail.com>
2021-05-20 18:15:30 +0900

    Update SearchMovie

---
+++ final_pjt/client/src/views/SearchMovieDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>{{ movie.title }}</h3>
+    <p>{{ movie.original_title }}</p>
+    <img :src="movie_poster" alt="" style="width: 30%;">
+    <p>감독</p>
+    <p>주연 배우</p>
+    <p>줄거리 : {{ movie.overview }}</p>
+    <hr>
+    <p>{{movie}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchMovieDetail',
+  computed: {
+    movie: function () {
+      return this.$store.state.detail
+    },
+    movie_poster: function () {
+      return 'https://www.themoviedb.org/t/p/original/' + this.movie.poster_path
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ final_pjt/client/src/views/SearchMovieList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <SearchMovieListItem
+      v-for="(movie, idx) in movies"
+      :key="idx"
+      :movie="movie"
+    />
+  </ul>
+</template>
+
+<script>
+import SearchMovieListItem from '@/components/SearchMovieListItem'
+
+export default {
+  name: 'SearchMovieList',
+  components: {
+    SearchMovieListItem,
+  },
+  computed: {
+    movies: function () {
+      return this.$store.state.searchs
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
