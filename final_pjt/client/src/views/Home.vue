7fed6ba1ab607f03384acdb44ec21d0a706ed20f
오승철 <tmdcjf103@gmail.com>
2021-05-20 14:36:16 +0900

    Add client

---
+++ final_pjt/client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <form action="">
+      <input type="text">
+      <button>검색</button>
+    </form>
+
+    <div id="now-playing">
+      <h2>현재 상영작</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
